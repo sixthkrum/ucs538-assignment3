e1d3dd488f9a3eafa1ddd67bdbd8850153f1dc3e
sixthkrum <mailbox.vikramalagh@gmail.com>
2020-09-17 12:16:58 +0530

    changed file tree and cleaned things up massize update not a good commit message

--- compressor/appCompressor.py
+++ compressor/appCompressor.py
@@ -1,6 +1,6 @@
 from flask import Flask
 
 app = Flask(__name__, template_folder = "templates")
-app.secret_key = "3920840329482388"
-app.config['UPLOAD_FOLDER'] = '/home/user/prog'
+app.secret_key = "2394723023940"
+app.config['UPLOAD_FOLDER'] = '/home/user/apps/compressor/uploads'
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
